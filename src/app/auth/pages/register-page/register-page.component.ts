38240e4ed1214e1e191d316b611a39a6ea879c00
jota santos <gestald118@email.com>
2024-10-01 08:51:56 -0400

    252. Formulario de Registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.email]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  constructor (private formBuilder: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: obter validação por um service
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
