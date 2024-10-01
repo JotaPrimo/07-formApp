850be9ad13571064b18e83a9bd0e00471e47cb4a
Jota Santos <jailsonsantosdpge@outlook.com>
2024-10-01 14:18:35 -0400

    255. Validator Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidations from 'src/app/shared/validators/validators';
+import { ValidatorService } from 'src/app/shared/services/validators.service';
+// import * as customValidations from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,16 +11,20 @@ import * as customValidations from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.pattern(customValidations.firstNameAndLastnamePattern) ]],
-    email: ['', [Validators.email, Validators.pattern(customValidations.emailPattern)]],
-    username: ['', [Validators.required, customValidations.cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern) ]],
+    email: ['', [Validators.email, Validators.pattern(this.validatorService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
-  constructor (private formBuilder: FormBuilder) {}
+  constructor (
+    private formBuilder: FormBuilder,
+    private validatorService: ValidatorService
+  ) {}
 
   isValidField(field: string) {
     // TODO: obter validação por um service
+    this.validatorService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
