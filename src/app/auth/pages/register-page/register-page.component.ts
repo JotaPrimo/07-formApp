66bf9a293370d651568c2f24adef37ec7e7417f3
Jota Santos <jailsonsantosdpge@outlook.com>
2024-10-01 13:23:09 -0400

    validators functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -11,7 +12,7 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email]],
-    username: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
